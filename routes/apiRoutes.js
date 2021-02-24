5e719c45adadb32f7c0cb2b1b2ef65f23355fc87
Norbu Sherpa <norbuts@hotmail.com>
2021-02-24 22:33:07 +1100

    updated save functionality adn addes db to gitignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,12 @@ const { json } = require("body-parser");
 
 module.exports = (app) => {
   // Retrieve all notes
-  app.get("/api/notes", (req, res) => res.json(db));
+  app.get("/api/notes", (req, res) => {
+    const notes = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../db/db.json"))
+    );
+    res.json(notes);
+  });
 
   // Retrive one note
   app.get("/api/notes/:id", (req, res) => {
@@ -21,14 +26,19 @@ module.exports = (app) => {
       length: 32,
       useLetters: false,
     });
-    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
+    const notes = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../db/db.json"))
+    );
     notes.push({
       id: id,
       title: req.body.title,
       text: req.body.text,
     });
 
-    fs.writeFileSync("../db/db.json", JSON.stringify(notes));
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notes)
+    );
     console.log("New note created", req.body);
     res.json({});
   });
